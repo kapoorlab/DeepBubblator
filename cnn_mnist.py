a06f9d77d4cd7d54abebdbad9668625407385945
kapoorlab <kapoorconsciousness@gmail.com>
2017-12-26 19:51:14 +0100

    Train and Eval MNist dataset

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -40,7 +40,7 @@ ImagesizeY = 28
 
 print("Channels: %s BatchSize %s" % (N_channels, BatchSize))
 
-
+ 
 def cnn_model_fn(features, labels, mode):
  """ Model function for CNN."""
  #Input Layer
@@ -66,7 +66,7 @@ def cnn_model_fn(features, labels, mode):
  
  #Build a dense layer by making a flat pooling layer
  
- pool2_flat = tf.reshape(pool2, 7*7*64)
+ pool2_flat = tf.reshape(pool2, [BatchSize, 7*7*64])
  
  dense = tf.layers.dense(inputs = pool2_flat, units= 1024, activation=tf.nn.relu)
  
@@ -83,11 +83,60 @@ def cnn_model_fn(features, labels, mode):
      return tf.estimator.EstimatorSpec(mode = mode, predictions = predictions)
  
  
+ "Loss function"
+ 
+ onehot_labels = tf.one_hot(indices= tf.cast(labels, tf.int32), depth = 10)
+ loss = tf.losses.softmax_cross_entropy(onehot_labels = onehot_labels, logits = logits)
+ 
+ "Here comes the SGD"
+ if mode == tf.estimator.ModeKeys.TRAIN:
+     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+     train_op = optimizer.minimize(loss = loss, global_step = tf.train.get_global_step())
+     return tf.estimator.EstimatorSpec(mode = mode, loss = loss, train_op = train_op)
  
  
-# Application logic
+ if mode == tf.estimator.ModeKeys.EVAL:
+ # Add evaluation metrics (for EVAL mode)
+     eval_metric_ops = {
+         "accuracy": tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"])}
+     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def main(unused_argv):
+     
+     mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+     train_data = mnist.train.images
+     train_labels = np.asarray(mnist.train.labels, dtype = np.int32)
+     eval_data = mnist.test.images
+     eval_labels = np.asarray(mnist.test.labels, dtype = np.int32)
+     mnist_classifier = tf.estimator.Estimator(model_fn = cnn_model_fn, model_dir="/Users/varunkapoor/mnist_convnet_model")     
+     tensors_to_log = {"probabilities": "softmax_tensor"}
+     logging_hook = tf.train.LoggingTensorHook(
+      tensors=tensors_to_log, every_n_iter=50)
+     #Train the model
+     train_input_fn = tf.estimator.inputs.numpy_input_fn(
+     x={"x": train_data},
+     y=train_labels,
+     batch_size=100,
+     num_epochs=None,
+     shuffle=True)
+     mnist_classifier.train(
+     input_fn=train_input_fn,
+     steps=1,
+     hooks=[logging_hook])
+ # Evaluate the model and print results
+     eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": eval_data},
+      y=eval_labels,
+      num_epochs=1,
+      shuffle=False)
+     eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+     print(eval_results) 
+ 
 
 if __name__ == "__main__":
+     print(__name__)
+
      tf.app.run()
   
     
